e165da157eedc41490bd5a856f89e4615f02d189
Luan Mollakuqe <luan.mollakuqe@gmail.com>
2021-01-10 17:46:36 +0100

    added all tasks

---
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -0,0 +1,37 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+
+    // calculate price, ratio, upper- and lowerbound
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const bound_limit = 0.05
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + bound_limit;
+    const lowerBound = 1 - bound_limit;
+
+    // return data to graph componentDiUpdate function
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
+  }
+}
